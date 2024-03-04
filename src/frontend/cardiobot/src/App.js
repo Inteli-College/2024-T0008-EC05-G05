23b30ef0da4ecb2337a94ec435a4455620ac1c04
edu-porto <eduardo.porto@sou.inteli.edu.br>
2024-03-04 17:30:46 -0300

    FEAT Template do react

---
+++ src/frontend/cardiobot/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // Import Routes
+
+import Dashboard from './pages/dashboard/dashboard.js';
+import Supplies from './pages/supplies/supplies.js';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/supplies" element={<Supplies />} />
+          <Route path="/" element={<Home />} /> {/* Define a Home component for the homepage */}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+// Define a Home component to render when no specific route is matched
+function Home() {
+  return (
+    <div>
+      <nav>
+      <h1>Bem vindo a página inicial</h1>
+        <ul>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/supplies">Supplies</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/frontend/cardiobot/src/pages/supplies/supplies.js
@@ -0,0 +1,13 @@
+// Supplies component
+import React from 'react';
+
+const Supplies = () => {
+  return (
+    <div>
+      <h2>Hello Supplies</h2>
+      {/* Add your supplies content here */}
+    </div>
+  );
+}
+
+export default Supplies;
