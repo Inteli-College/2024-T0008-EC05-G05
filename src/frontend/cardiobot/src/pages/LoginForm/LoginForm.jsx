f74f3325530053842f22e717948cb4e9ceb7b18f
MarioVM <mario.medeiros@sou.inteli.edu.br>
2024-03-14 17:31:17 -0300

    att tela de login
    melhora na UX da tela de login

--- src/frontend/cardiobot/src/pages/LoginForm/LoginForm.jsx
+++ src/frontend/cardiobot/src/pages/LoginForm/LoginForm.jsx
@@ -5,32 +5,35 @@ import { FaUser, FaLock } from "react-icons/fa";
 const LoginForm = () => {
     return (
         <div className='master'>
-            <div className='wrapper'>
-                <from action="">
-                    <h1>Login</h1>
-
-                    <div className='input-box'>
-                        <input type='text' placeholder='Usuário' required />
-                        <FaUser className='icon' />
-                    </div>
-
-                    <div className='input-box'>
-                        <input type='password' placeholder='Senha' required />
-                        <FaLock className='icon' />
-                    </div>
-
-                    <div className='remember-forgot'>
-                        <label>
-                            <input type='checkbox' />lembrar de mim
-                        </label>
-                    </div>
-                    <div>
-                        <button type='submit'>Login</button>
-                    </div>
-
-
-                </from>
+            <div className='logo'>
+                <h2>Cardio-Bot</h2>
             </div>
+            <main className='container'>
+                <div className='wrapper'>
+                    <from action="">
+                        <h1>Login</h1>
+
+                        <div className='input-box'>
+                            <input type='text' placeholder='Usuário' required />
+                            <FaUser className='icon' />
+                        </div>
+
+                        <div className='input-box'>
+                            <input type='password' placeholder='Senha' required />
+                            <FaLock className='icon' />
+                        </div>
+
+                        <div className='remember-forgot'>
+                            <label>
+                                <input type='checkbox' />lembrar de mim
+                            </label>
+                        </div>
+                        <div>
+                            <button type='submit'>Login</button>
+                        </div>
+                    </from>
+                </div>
+            </main>
         </div>
     )
 }
