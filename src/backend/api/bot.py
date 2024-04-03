fb109e1652365505c6205b1b8c7956a3b8ed7029
Caio Paula <caio.paula@sou.inteli.edu.br>
2024-04-03 20:53:21 -0300

    fit: corrigindo bugs the portas

--- src/backend/api/bot.py
+++ src/backend/api/bot.py
@@ -332,6 +332,6 @@ async def salvar_posicao(position_code: str,):
 if __name__ == "__main__":
     try:
         import uvicorn
-        uvicorn.run(app, host="127.0.0.1", port=8800, debug=True, reload=True, log_level="info")
+        uvicorn.run(app, host="127.0.0.1", port=8800)
     except ImportError as e:
         print(e)
